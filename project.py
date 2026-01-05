2572ab3bd739c90ac158f660879db90eb03e6551
danielkim321 <dkthor2008@gmail.com>
2026-01-05 08:52:41 -0500

    78

--- project.py
+++ project.py
@@ -121,4 +121,5 @@ while True:
         screen.fill(BG)
         screen.blit(txt, (W // 2 - txt.get_width() // 2, H // 2 - 40))
         pygame.display.flip()
-        clock.tick(390)
+        clock.tick(390
+                   )
