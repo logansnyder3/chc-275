a5a528a430a1ef8132c69eea47b3cfed14a2e9ee
danielkim321 <dkthor2008@gmail.com>
2026-01-20 10:13:19 -0500

    1

---
+++ functions.py
@@ -0,0 +1,22 @@
+def foo():
+    print("bar")
+foo() 
+
+
+foo()
+def ad5(num): 
+    num = num + 5 
+    print(num)
+
+add5(8)
+add5(4)
+
+mynum = 10
+add5(mynum)
+
+def multiply(num1, num2):
+    product = num1*num2
+    print(product)
+multiply (3,5)
+multiply(4,10)
+multiply (4,"a")
