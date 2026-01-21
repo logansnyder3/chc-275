c2ad3d1e0739a0778a2bbff127a3c5e91afc911b
danielkim321 <dkthor2008@gmail.com>
2026-01-21 09:10:48 -0500

    78

--- functions.py
+++ functions.py
@@ -1,22 +1,4 @@
-def foo():
-    print("bar")
-foo() 
-
-
-foo()
-def ad5(num): 
-    num = num + 5 
-    print(num)
-
-add5(8)
-add5(4)
-
-mynum = 10
-add5(mynum)
-
-def multiply(num1, num2):
-    product = num1*num2
-    print(product)
-multiply (3,5)
-multiply(4,10)
-multiply (4,"a")
+def foo(x,y):
+    print(x + y)
+foo("foo","bar")
+foo(6,7)
