78316773052f3fc7cca87cc492d0025eda61d967
danielkim321 <dkthor2008@gmail.com>
2026-02-10 09:41:25 -0500

    j

--- design.py
+++ design.py
@@ -1,1 +1,26 @@
+ def(main)
  
+ 
+ if option == "4":
+    
+    x = 5
+
+def foo():
+    print("hello")
+    
+y = 6
+def bar():
+    print("bar")
+    
+import functions
+
+    
+def main():
+    x = 5 
+    y = 6 
+    foo()
+    functions.add5(x)
+
+if __name__ == "__main__": 
+    main()
+    
