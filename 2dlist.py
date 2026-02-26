8d4fbc7f29928986dd62f1d55f58c89ad4e3c7de
danielkim321 <dkthor2008@gmail.com>
2026-02-26 08:54:39 -0500

    8

---
+++ 2dlist.py
@@ -0,0 +1,30 @@
+myList [1, 2, 3,]
+
+grid = [
+    [0,0,0,0],
+    [0,0,0,0],
+    [0,0,0,0],
+]
+
+print(grid[0][0])
+
+grid2 = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+print(grid2[1][2])
+
+i = 0
+while i < len(nums):
+    
+for j in range(len(grid2))
+print 
+print(f'({i},{j}) = 
+      
+      
+for i in range(len(board)):
+    print("|", end = ")
+    print(board[i])
+
+board[row][col] = current_player
